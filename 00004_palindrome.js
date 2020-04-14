b5121dc05f9899b34ee4a7f9454403f8719614f9
Ritheesh <ritheesh.4@gmail.com>
2020-04-14 09:25:04 +0530

    palindrome implemented

--- 00004_palindrome.js
+++ 00004_palindrome.js
@@ -1,1 +1,15 @@
+// 4. Enter a  string and find if it's a palindrome
+
 const readLine = require('readline-sync');
+const inputString = readLine.question("Enter the string to check palindrome property: ");
+
+let palindromeTesting = (inputString) => {
+    let reverOftheString = inputString.split('').reverse().join('');
+    if (inputString === reverOftheString){
+        console.log("The input string is a palindrome");
+    }else {
+        console.log("The input string is not a palindrome");
+    }
+}
+
+palindromeTesting(inputString);
