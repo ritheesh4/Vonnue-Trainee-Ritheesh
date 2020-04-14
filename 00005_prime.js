08f8f6a0e16a378a97e11acde55768c82638c87a
Ritheesh <ritheesh.4@gmail.com>
2020-04-14 10:53:29 +0530

    file name changed

---
+++ 00005_prime.js
@@ -0,0 +1,26 @@
+// 5. Enter a number and find if it's prime
+
+const readLine = require('readline-sync');
+const inputNumber = readLine.question("Input the number to check wether it is a prime number or not: ");
+
+let primeNumberTesint = () => {
+    if (inputNumber < 2) {
+        console.log("The input number is not a pime.")
+    } else {
+        let flagForPrimeConclusion = 0;
+        let i = 1;
+        while (flagForPrimeConclusion == 0) {
+            i++;
+            if (i < inputNumber) {
+                if (inputNumber % i === 0) {
+                    console.log("The input number is not a prime.");
+                    flagForPrimeConclusion = 1;
+                }
+
+            } else { flagForPrimeConclusion = 2; }
+        }
+        if (flagForPrimeConclusion == 2) { console.log("The input number is a prime."); }
+    }
+}
+
+primeNumberTesint(inputNumber);
