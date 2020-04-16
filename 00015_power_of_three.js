534dbb682a85ce76c16c4294cdbc9dcfc8c31bf9
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 16:04:28 +0530

    Initial commit

---
+++ 00015_power_of_three.js
@@ -0,0 +1,14 @@
+// 15. Write program to check if number is power of 3 of any number
+
+let inputNumber;
+
+const powerOfThreeTesting = (inputNumber) => {
+    if (inputNumber % 3 > 0) {
+        return 'Not power of 3'
+    }
+    else {
+        return 'Power of 3'
+    }
+}
+
+powerOfThreeTesting(inputNumber)
