b943fb5551fbbdbe44aafe0442b035ad2b14b599
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 11:12:48 +0530

    readline removed.Function into const.

--- 00004_palindrome.js
+++ 00004_palindrome.js
@@ -1,9 +1,7 @@
 // 4. Enter a  string and find if it's a palindrome.
 
-const readLine = require('readline-sync');
-const inputString = readLine.question("Enter the string to check wether it is palindrome or not : ");
-
-let palindromeTesting = (inputString) => {
+let inputString;
+const palindromeTesting = (inputString) => {
     let reverseOfString = inputString.split('').reverse().join('');
     if (inputString === reverseOfString) {
         return "Palindrome"
