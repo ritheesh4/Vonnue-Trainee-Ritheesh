7c69c05b10e6316a45bd65917776eb4934f38a92
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 12:42:05 +0530

    Declared i,return and argument added,changed function to const.

--- 00008_sum_of_n_numbers.js
+++ 00008_sum_of_n_numbers.js
@@ -1,14 +1,13 @@
 // 8. Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
 
-const readLine = require('readline-sync');
-const n = parseFloat(readLine.question("Enter the limit value of n to find the sum: "));
+let n;
 
-let sum = () => {
+const sum = (n) => {
     let sumOfLimit = 0;
-    for (i = 1; i < n+1; i++) {
+    for (let i = 1; i < n + 1; i++) {
         sumOfLimit = sumOfLimit + i;
     }
-    console.log("Sum of n numbers: ", sumOfLimit)
+    return sumOfLimit
 }
 
 sum(n);
