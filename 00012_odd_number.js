d5e9da7540ab4e4c1e31718371af7e0da7762439
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 21:24:10 +0530

    console added

--- 00012_odd_number.js
+++ 00012_odd_number.js
@@ -4,11 +4,13 @@ let inputNumber;
 
 const oddNumberTest = (inputNumber) => {
     if (inputNumber % 2 == 0) {
-        return "not odd"
+        return "Not odd. Its even."
     } else {
         return "odd"
     }
 }
 
 oddNumberTest(inputNumber);
+console.log("The input number is: ",oddNumberTest(3));
+console.log("The input number is: ",oddNumberTest(4));
 
