e3f93c836c4ff753f31bfbdf7d44edd152069ec3
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 21:35:01 +0530

    simplified code. Added console

--- 00014_number_as_sum_square_route_two_numbers.js
+++ 00014_number_as_sum_square_route_two_numbers.js
@@ -1,29 +1,22 @@
 // 14. Write program to check if number can be expressed as sum of squares of two whole numbers
 
-const readLine = require('readline-sync');
-const inputNumber = parseInt(readLine.question("Enter the number: "));
 
-let flagOfSumSquare = 0;
-let sqrt = Math.sqrt(inputNumber);
-let sqrtOfInputNumber = Math.floor(sqrt)
-let repeatI = 0;
-let repeatJ = 0;
+let inputNumber;
 
 const checkSumOfSquares = (inputNumber) => {
+    let sqrt = Math.sqrt(inputNumber);
+    let sqrtOfInputNumber = Math.floor(sqrt);
     for (let i = 1; i < sqrtOfInputNumber + 1; i++) {
         for (let j = 1; j < sqrtOfInputNumber + 1; j++) {
             let squareSum = (i * i) + (j * j);
             if (squareSum == inputNumber) {
-                if (repeatI != j) {
-                    console.log("This number can be expressed as sum of squares of", i, j);
-                    flagOfSumSquare = 1;
-                    repeatI = i;
-                    repeatJ = j;
-                }
+                return 'True'
             }
         }
     }
-    if (flagOfSumSquare == 0) { console.log("This number can not be expresses as sum of squares of two numbers.") }
+    return 'False'
 }
 
 checkSumOfSquares(inputNumber);
+console.log("The input number can be expressed as sum of squares of two whole numbers:", checkSumOfSquares(5));
+console.log("The input number can be expressed as sum of squares of two whole numbers:", checkSumOfSquares(6));
