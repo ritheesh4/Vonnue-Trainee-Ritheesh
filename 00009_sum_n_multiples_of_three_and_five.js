7dd4461c4ed83807a529ac9456284d0e1a42f002
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 12:45:23 +0530

    i declared, function changed to const,return and argument added

--- 00009_sum_n_multiples_of_three_and_five.js
+++ 00009_sum_n_multiples_of_three_and_five.js
@@ -1,16 +1,15 @@
 // 9. Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
 
-const readLine = require('readline-sync');
-const n = parseFloat(readLine.question("Enter the limit value of n to find the sum: "));
+let n;
 
-let sum = () => {
+const sum = (n) => {
     let sumOfLimit = 0;
-    for (i = 3; i < n + 1; i++) {
+    for (let i = 3; i < n + 1; i++) {
         if (i % 3 == 0 || i % 5 == 0) {
             sumOfLimit = sumOfLimit + i;
         }
     }
-    console.log("Sum of multiples of three or five n numbers: ", sumOfLimit)
+    return sumOfLimit
 }
 
 sum(n);
