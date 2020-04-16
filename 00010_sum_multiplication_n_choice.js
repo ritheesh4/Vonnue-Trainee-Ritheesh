021b3273c7e410dceca30484eb6a030ccb27d594
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 21:12:55 +0530

    console added

--- 00010_sum_multiplication_n_choice.js
+++ 00010_sum_multiplication_n_choice.js
@@ -1,7 +1,17 @@
 // 10. Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.
 
 let n;
-let choiceOfUser; /*1.Muliplication or 2.Sum.*/
+let choiceOfUser; /*1.Muliplication or 2.Sum. Any other input is invalid.*/
+
+const choiseOfUserInput = (choiceOfUser, n) => {
+    if (choiceOfUser == 1) {
+        return multiplicaition(n)
+    } else if (choiceOfUser == 2) {
+        return sum(n)
+    } else {
+        return invalidInput()
+    }
+}
 
 const sum = (n) => {
     let sumOfLimit = 0;
@@ -23,11 +33,10 @@ const invalidInput = () => {
     return "Invalid input"
 }
 
-if (choiceOfUser == 1) {
-    multiplicaition(n)
-} else if (choiceOfUser == 2) {
-    sum(n)
-} else {
-    invalidInput()
-}
+sum(n)
+multiplicaition(n)
+choiseOfUserInput(choiceOfUser, n)
+console.log("Sum of 10 numbers", choiseOfUserInput(2, 10));
+console.log("Multiplication of 10 numbers", choiseOfUserInput(1, 10));
+console.log("Multiplication of 10 numbers", choiseOfUserInput(3, 10));
 
