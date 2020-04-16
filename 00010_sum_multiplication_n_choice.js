bd39ff7c14fa6bd4752615cc3ce342c73d4d8cd1
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 12:52:40 +0530

    Argument and return added. i declared,readLine removed and a new function declared for invalid input

--- 00010_sum_multiplication_n_choice.js
+++ 00010_sum_multiplication_n_choice.js
@@ -1,23 +1,26 @@
 // 10. Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.
 
-const readLine = require('readline-sync');
-const n = parseFloat(readLine.question("Enter the limit value of n: "));
-let choiceOfUser = parseFloat(readLine.question("1.Muliplication or 2.Sum. Input the option number you prefer: "));
+let n;
+let choiceOfUser; /*1.Muliplication or 2.Sum.*/
 
-let sum = () => {
+const sum = (n) => {
     let sumOfLimit = 0;
-    for (i = 1; i < n + 1; i++) {
+    for (let i = 1; i < n + 1; i++) {
         sumOfLimit = sumOfLimit + i;
     }
-    console.log("Sum of n numbers: ", sumOfLimit)
+    return sumOfLimit
 }
 
-let multiplicaition = () => {
+const multiplicaition = (n) => {
     let multiplicationOfLimit = 1;
-    for (i = 1; i < n + 1; i++) {
+    for (let i = 1; i < n + 1; i++) {
         multiplicationOfLimit = multiplicationOfLimit * i;
     }
-    console.log("Multiplied result of n numbers: ", multiplicationOfLimit)
+    return multiplicationOfLimit
+}
+
+const invalidInput = () => {
+    return "Invalid input"
 }
 
 if (choiceOfUser == 1) {
@@ -25,6 +28,6 @@ if (choiceOfUser == 1) {
 } else if (choiceOfUser == 2) {
     sum(n)
 } else {
-    console.log("Input a valid option.")
+    invalidInput()
 }
 
