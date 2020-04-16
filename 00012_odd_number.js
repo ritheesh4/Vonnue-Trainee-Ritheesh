384970fece2f038afa3c5309d838b75bcfd23ac3
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 13:07:42 +0530

    function let changed to const,return implemented,argumet passed and readline removed

--- 00012_odd_number.js
+++ 00012_odd_number.js
@@ -1,13 +1,12 @@
 // 13. Write a program to check if number is odd
 
-const readLine = require('readline-sync');
-let inputNumber = parseInt(readLine.question("Input the number to check wether it is odd number or not: "));
+let inputNumber;
 
-let oddNumberTest = () => {
+const oddNumberTest = (inputNumber) => {
     if (inputNumber % 2 == 0) {
-        console.log("Input number is not odd.")
+        return "not odd"
     } else {
-        console.log("Input number is an odd number.");
+        return "odd"
     }
 }
 
