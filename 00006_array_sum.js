401cb8c67917bd960cbdb9702b6a1f15573fa84e
Ritheesh <ritheesh.4@gmail.com>
2020-04-16 12:31:21 +0530

    i declared.Return and argument added.Readline Removed

--- 00006_array_sum.js
+++ 00006_array_sum.js
@@ -1,14 +1,13 @@
 // 6. Find the sum of elements in a given array. Array should be an input to the program
 
-const readLine = require('readline-sync');
-const inputArray = readLine.question("Enter the array elemets. Seperate them using comma: ").split(",");
+let inputArray;
 
-let sumOfArrayElements = () => {
+const sumOfArrayElements = (inputArray) => {
     let result = 0;
-    for (i = 0; i < inputArray.length; i++) {
+    for (let i = 0; i < inputArray.length; i++) {
         result = result + parseFloat(inputArray[i]);
     }
-    console.log("Sum of array elements = ", result);
+    return result
 }
 
 sumOfArrayElements(inputArray);
