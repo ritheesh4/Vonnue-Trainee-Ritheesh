87da8156e25a5b49ad0331e6a470a8488f19dc85
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 22:10:49 +0530

    Initial commit

---
+++ 00036_fibonacii.js
@@ -0,0 +1,21 @@
+// 36. Fibonacci series up ton
+
+let n;
+let fibonacciResult = [];
+
+const fibonacci = (n) => {
+    fibonacciResult[0] = 0;
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            fibonacciResult[i] = fibonacciResult[i] + i;
+        } else if (i === 1) {
+            fibonacciResult[i] = fibonacciResult[i - 1] + i;
+        } else {
+            fibonacciResult[i] = fibonacciResult[i - 1] + fibonacciResult[i - 2];
+        }
+    }
+    return fibonacciResult
+}
+
+fibonacci(n);
+console.log("The fibonacci series of value n =10 : ", fibonacci(10));
