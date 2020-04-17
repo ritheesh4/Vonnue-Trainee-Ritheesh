85cb752e8ea1fb5f5ca791ad3163398bd3fe4914
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 08:56:06 +0530

    equal to added

--- 00005_prime.js
+++ 00005_prime.js
@@ -8,7 +8,7 @@ const primeNumberTesint = (inputNumber) => {
     } else {
         let flagForPrimeConclusion = 0;
         let i = 1;
-        while (flagForPrimeConclusion == 0) {
+        while (flagForPrimeConclusion === 0) {
             i++;
             if (i < inputNumber) {
                 if (inputNumber % i === 0) {
@@ -19,7 +19,7 @@ const primeNumberTesint = (inputNumber) => {
                 flagForPrimeConclusion = 2;
             }
         }
-        if (flagForPrimeConclusion == 2) {
+        if (flagForPrimeConclusion === 2) {
             return "Prime"
         }
     }
