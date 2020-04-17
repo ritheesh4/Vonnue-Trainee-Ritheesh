5e503c1ee41f775027653b4c4250e36904319781
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 23:45:38 +0530

    Initial commit

---
+++ 00040_perfect_number.js
@@ -0,0 +1,31 @@
+// 40. Perfect number or not
+
+let inputNumber;
+let perfectNumberArray = [];
+let indexValue = 0;
+let sumOfArray = 0;
+
+const perfectNumber = (inputNumber) => {
+    for (let i = 0; i < inputNumber; i++) {
+        if (inputNumber % i === 0) {
+            perfectNumberArray[indexValue] = i;
+            indexValue++;
+        }
+    }
+    if (perfectNumberArray[0] !== 0) {
+        for (let j = 0; j < perfectNumberArray.length; j++) {
+            sumOfArray = sumOfArray + perfectNumberArray[j];
+        }
+        if (sumOfArray === inputNumber) {
+            return "Perfect number"
+        } else {
+            return "Not perfect number"
+        }
+    } else {
+        return "Not perfect number"
+    }
+}
+
+perfectNumber(inputNumber);
+console.log("The input number 496 is : ", perfectNumber(496));
+console.log("The input number 24 is : ", perfectNumber(24));
