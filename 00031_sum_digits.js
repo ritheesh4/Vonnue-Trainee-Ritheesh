2b9206f527e702b7bc5f927cc901393541085126
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 20:48:33 +0530

    Initial commit

---
+++ 00031_sum_digits.js
@@ -0,0 +1,15 @@
+// 31. Sum of digits of a number
+
+let inputNumber = 0;
+let sum = 0;
+
+const sumOfDigits = (inputNumber) => {
+    while (inputNumber) {
+        sum += inputNumber % 10;
+        inputNumber = Math.floor(inputNumber / 10);
+    }
+    return sum
+}
+
+sumOfDigits(inputNumber);
+console.log("Sum of digits of input number 345: ", sumOfDigits(345));
