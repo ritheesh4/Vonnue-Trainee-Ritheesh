0a4d803bdaf9b4f55b1cb9f34a80f0921285a9ff
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 19:59:26 +0530

    initial commit

---
+++ 00028_greatest_three.js
@@ -0,0 +1,24 @@
+// 28. Greatest of two numbers
+
+let number1;
+let number2;
+let number3;
+
+const greatestOfThree = (number1, number2, number3) => {
+    if (number1 - number2 > 0) {
+        if (number1 - number3 > 0) {
+            return number1
+        } else {
+            return number3
+        }
+    } else {
+        if (number2 - number3 > 0) {
+            return number2
+        } else {
+            return number3
+        }
+    }
+}
+
+greatestOfThree(number1, number2, number3);
+console.log("The greatest number of 11, 54, 22 is : ", greatestOfThree(11, 54, 22));
