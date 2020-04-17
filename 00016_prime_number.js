39766f1e9b58f12bbe7376244095bb778a16684e
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 12:51:16 +0530

    space between question and program added

--- 00016_prime_number.js
+++ 00016_prime_number.js
@@ -1,4 +1,5 @@
 // 16. Write a program that prints all prime numbers. (Note: if your programming language does not support arbitrary size numbers, printing all primes up to the largest number you can easily represent is fine too.)
+
 let primeNumberArray = [];
 let primeNumberArrayIndexValue = 1;
 let primaryNumberFlag = 0;
