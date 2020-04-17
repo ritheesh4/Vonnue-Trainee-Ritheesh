812151f33f2bb5d2cb8396b313decfd71348ebbc
Ritheesh <ritheesh.4@gmail.com>
2020-04-17 21:40:44 +0530

    Initial commit

---
+++ 00034_reverse_number.js
@@ -0,0 +1,16 @@
+// 34. Reverse a given number
+
+let inputNumber = 0;
+let reverseResult = 0;
+
+const reverseNumber = (inputNumber) => {
+    while (inputNumber) {
+        reverseResult = reverseResult * 10;
+        reverseResult += inputNumber % 10;
+        inputNumber = Math.floor(inputNumber / 10);
+    }
+    return reverseResult
+}
+
+reverseNumber(inputNumber);
+console.log("The revers of input number 256: ", reverseNumber(256));
