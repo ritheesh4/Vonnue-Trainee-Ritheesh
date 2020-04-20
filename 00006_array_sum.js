8d636e3ef55bc909b4a7f526235043e08dc1be99
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:36:45 +0530

    remove unwanted variable declaration. edited console message

--- 00006_array_sum.js
+++ 00006_array_sum.js
@@ -1,13 +1,11 @@
 // 6. Find the sum of elements in a given array. Array should be an input to the program
 
-let inputArray = [];
-
 const sumOfArrayElements = (inputArray) => {
     let result = 0;
     for (let i = 0; i < inputArray.length; i++) {
         result = result + inputArray[i];
     }
-    return result
-}
+    return result;
+};
 
-console.log("sum of arrays: ", sumOfArrayElements([5, 6, 3, 2]));
+console.log("sum of array elements: ", sumOfArrayElements([5, 6, 3, 2]));
