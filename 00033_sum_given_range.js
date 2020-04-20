eaf010bbfc1fbf781d563bf123d47e604d7958ad
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 15:52:23 +0530

    Removed unwanted variable declaration and added semicolon

--- 00033_sum_given_range.js
+++ 00033_sum_given_range.js
@@ -1,14 +1,12 @@
 // 33. Sum of numbers in a given range
 
 let result = 0;
-let startingRange = 0;
-let endingRange = 0;
 
 const sumGivenRange = (startingRange, endingRange) => {
     for (let i = startingRange; i < endingRange + 1; i++) {
         result = result + i;
     }
-    return result
-}
+    return result;
+};
 
 console.log("Printing sum of numbers in a range of 4 and 12 : ", sumGivenRange(4, 12));
