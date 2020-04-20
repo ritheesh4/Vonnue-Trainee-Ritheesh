c83f2a68344ec26fa8c252bdd00642f6710909e1
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 15:58:45 +0530

    Removed unwanted variable declaration and added semicolon

--- 00029_greatest_three.js
+++ 00029_greatest_three.js
@@ -1,23 +1,19 @@
 // 28. Greatest of two numbers
 
-let number1;
-let number2;
-let number3;
-
 const greatestOfThree = (number1, number2, number3) => {
     if (number1 - number2 > 0) {
         if (number1 - number3 > 0) {
-            return number1
+            return number1;
         } else {
-            return number3
+            return number3;
         }
     } else {
         if (number2 - number3 > 0) {
-            return number2
+            return number2;
         } else {
-            return number3
+            return number3;
         }
     }
-}
+};
 
 console.log("The greatest number among 11, 54 and 22 is : ", greatestOfThree(11, 54, 22));
