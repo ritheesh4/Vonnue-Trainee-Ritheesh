5ffdda60cd578928d939d5b139645cb56273e6cd
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:24:23 +0530

    removed unwanted variable declaration

--- 00012_odd_number.js
+++ 00012_odd_number.js
@@ -1,15 +1,12 @@
 // 13. Write a program to check if number is odd
 
-let inputNumber;
-
 const oddNumberTest = (inputNumber) => {
     if (inputNumber % 2 === 0) {
-        return "Not odd. Its even."
+        return "Not odd. Its even.";
     } else {
-        return "odd"
+        return "odd";
     }
-}
+};
 
 console.log("The input number 3 is: ",oddNumberTest(3));
 console.log("The input number 4 is: ",oddNumberTest(4));
-
