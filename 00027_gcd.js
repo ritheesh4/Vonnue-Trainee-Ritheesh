d340143319b91eb79f00b69a9383a5b64c82649a
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:01:25 +0530

    removed unwanted variable declarations and added semicolon

--- 00027_gcd.js
+++ 00027_gcd.js
@@ -1,16 +1,13 @@
 // 27. GCD of two numbers
 
-let number1;
-let number2;
-
 const gcd = (number1, number2) => {
     for (let gcdValue = number1; gcdValue > 0; gcdValue--) {
         if (number1 % gcdValue === 0) {
             if (number2 % gcdValue === 0) {
-                return gcdValue
+                return gcdValue;
             }
         }
     }
-}
+};
 
 console.log("GCD of two numbers 98 and 56: ", gcd(98, 56));
