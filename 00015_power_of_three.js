cf24b63c7f6e6cb1a7077fc1668ee60b3002b3ec
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:20:18 +0530

    Removed unwanted variable declaration

--- 00015_power_of_three.js
+++ 00015_power_of_three.js
@@ -1,15 +1,13 @@
 // 15. Write program to check if number is power of 3 of any number
 
-let inputNumber;
-
 const powerOfThreeTesting = (inputNumber) => {
     if (inputNumber % 3 > 0) {
-        return 'Not power of 3'
+        return 'Not power of 3';
     }
     else {
-        return 'Power of 3'
+        return 'Power of 3';
     }
-}
+};
 
 console.log("The input number is 9: ", powerOfThreeTesting(9));
 console.log("The input number is 10: ", powerOfThreeTesting(10));
