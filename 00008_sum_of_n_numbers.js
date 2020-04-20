5583868981b4f32fb2415a86ef74f02f8f47d2c2
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:32:33 +0530

    Removed unwanted variable declaration and added semicolon

--- 00008_sum_of_n_numbers.js
+++ 00008_sum_of_n_numbers.js
@@ -1,13 +1,11 @@
-// 8. Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
-
-let n;
+// 8. Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
 
 const sum = (n) => {
     let sumOfLimit = 0;
     for (let i = 1; i < n + 1; i++) {
         sumOfLimit = sumOfLimit + i;
     }
-    return sumOfLimit
-}
+    return sumOfLimit;
+};
 
 console.log("Sum of 100 numbers:",sum(100));
