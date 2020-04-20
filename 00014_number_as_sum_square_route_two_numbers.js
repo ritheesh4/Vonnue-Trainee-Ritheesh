28df699f821a7a6b1e18a15b5e0a0371ea7107bc
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:21:47 +0530

    Removed unwanted variable declaration and added semicolon

--- 00014_number_as_sum_square_route_two_numbers.js
+++ 00014_number_as_sum_square_route_two_numbers.js
@@ -1,19 +1,17 @@
 // 14. Write program to check if number can be expressed as sum of squares of two whole numbers
 
-let inputNumber;
-
 const checkSumOfSquares = (inputNumber) => {
     let sqrt = Math.sqrt(inputNumber);
     let sqrtOfInputNumber = Math.floor(sqrt);
     for (let i = 1; i < sqrtOfInputNumber + 1; i++) {
         for (let j = 1; j < sqrtOfInputNumber + 1; j++) {
             if (((i * i) + (j * j)) === inputNumber) {
-                return 'True'
+                return 'True';
             }
         }
     }
-    return 'False'
-}
+    return 'False';
+};
 
 console.log("The input number 5 can be expressed as sum of squares of two whole numbers:", checkSumOfSquares(5));
 console.log("The input number 6 can be expressed as sum of squares of two whole numbers:", checkSumOfSquares(6));
