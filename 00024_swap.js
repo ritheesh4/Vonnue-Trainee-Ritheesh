0b0377604cc78b4428b0eceeff40508d1e06029b
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:06:13 +0530

    Removed unwanted variable declarations and added semicolon

--- 00024_swap.js
+++ 00024_swap.js
@@ -1,14 +1,10 @@
 // 24. Write a program to Swap two numbers without third variable.
 
-let inputNumber1;
-let inputNumber2;
-
 const swapVariables = (inputNumber1, inputNumber2) => {
     inputNumber1 = inputNumber1 + inputNumber2;
     inputNumber2 = inputNumber1 - inputNumber2;
     inputNumber1 = inputNumber1 - inputNumber2;
-    return [inputNumber1, inputNumber2]
-
-}
+    return [inputNumber1, inputNumber2];
+};
 
 console.log("Swap of tw0 numbers 5,6:", swapVariables(5, 6));
