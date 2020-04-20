53519d9cfdd3dcd677c8f3b46d512a628455c057
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 12:36:36 +0530

    Removed unwanted function call

--- 00032_sum_natural_numbers.js
+++ 00032_sum_natural_numbers.js
@@ -4,12 +4,10 @@ let result = 0;
 let n = 0;
 
 const sum = (n) => {
-    let result = 0;
     for (let i = 1; i < n + 1; i++) {
         result = result + i;
     }
     return result
 }
 
-sum(n);
 console.log("Printing sum of natural numbers n = 10: ", sum(10));
