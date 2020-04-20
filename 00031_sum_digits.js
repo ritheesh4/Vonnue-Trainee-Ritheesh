3efb080d8abf1aa34a681e5ebf79f86a8d771a99
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 15:55:23 +0530

    removed unwanted variable declaration and added semicolon

--- 00031_sum_digits.js
+++ 00031_sum_digits.js
@@ -1,6 +1,5 @@
 // 31. Sum of digits of a number
 
-let inputNumber = 0;
 let sum = 0;
 
 const sumOfDigits = (inputNumber) => {
@@ -8,7 +7,7 @@ const sumOfDigits = (inputNumber) => {
         sum += inputNumber % 10;
         inputNumber = Math.floor(inputNumber / 10);
     }
-    return sum
-}
+    return sum;
+};
 
 console.log("Sum of digits of input number 345: ", sumOfDigits(345));
