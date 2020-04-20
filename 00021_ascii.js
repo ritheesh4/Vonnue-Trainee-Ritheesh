a5527c99aea14cf8ce8ef3e7cbe8d0abb27825bd
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:10:45 +0530

    Removed unwanted variable declaration and added semicolon

--- 00021_ascii.js
+++ 00021_ascii.js
@@ -1,12 +1,11 @@
 // 21. Write a program to check Ascii values of a character
 
-let inputCharacter = '';
 let asciiValueOfInput;
 
 const asciiValue = (inputCharacter) => {
     asciiValueOfInput = inputCharacter.charCodeAt();
-    return asciiValueOfInput
-}
+    return asciiValueOfInput;
+};
 
 console.log("ASCII value of the input character a is: ", asciiValue('d'));
 console.log("ASCII value of the input $ is: ", asciiValue('$'));
