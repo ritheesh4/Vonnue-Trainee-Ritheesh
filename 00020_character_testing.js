62b05accba0f04c4bcd149703fb20ea63ad1c12d
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:12:44 +0530

    Removed unwanted variable declaration and added semicolon

--- 00020_character_testing.js
+++ 00020_character_testing.js
@@ -1,16 +1,15 @@
 // 20. Write a program to check if a character is an alphabet or not
 
-let inputCharacter = '';
 let asciiValueOfInput;
 
 const alphabetOrNot = (inputCharacter) => {
     asciiValueOfInput = inputCharacter.charCodeAt();
     if (asciiValueOfInput < 65 || (asciiValueOfInput > 90 && asciiValueOfInput < 97) || asciiValueOfInput > 122) {
-        return "Not aplhaber"
+        return "Not Aplhabet";
     } else {
-        return "Alphabet"
+        return "Alphabet";
     }
-}
+};
 
 console.log("Input character a is: ", alphabetOrNot('a'));
 console.log("Input character $ is: ", alphabetOrNot('$'));
