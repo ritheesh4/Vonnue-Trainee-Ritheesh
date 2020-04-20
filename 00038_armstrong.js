5a670b4bbdf96b2aa35cc59ca97fa3118bbb3cf7
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 12:45:05 +0530

    Removed unwated function call and removed one variable

--- 00038_armstrong.js
+++ 00038_armstrong.js
@@ -1,7 +1,6 @@
 // 38. Armstrong number or not
 
 let inputNumber;
-let cubeSum;
 
 const checkArmstrong = (inputNumber) => {
     let sqrt = Math.cbrt(inputNumber);
@@ -9,8 +8,7 @@ const checkArmstrong = (inputNumber) => {
     for (let i = 1; i < cbrtOfInputNumber + 1; i++) {
         for (let j = 1; j < cbrtOfInputNumber + 1; j++) {
             for (let k = 0; k < cbrtOfInputNumber; k++) {
-                cubeSum = (i * i * i) + (j * j * j) + (k * k * k);
-                if (cubeSum === inputNumber) {
+                if (((i * i * i) + (j * j * j) + (k * k * k)) === inputNumber) {
                     return "True"
                 }
             }
@@ -19,6 +17,5 @@ const checkArmstrong = (inputNumber) => {
     return 'False'
 }
 
-checkArmstrong(inputNumber);
 console.log("The input number 371 is armstrong number:", checkArmstrong(371));
 console.log("The input number 150 is armstrong number:", checkArmstrong(150));
