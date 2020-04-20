931fde982953871ab0069041865ff2c48b98d8ca
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:04:49 +0530

    Removed unwanted variable declarations and added semicolon

--- 00025_circle_area.js
+++ 00025_circle_area.js
@@ -1,11 +1,8 @@
 // 25. Find Area of a circle
 
-let radius;
-let area;
-
 const areaOfCircle = (radius) => {
-    area = 3.14 * radius * radius;
-    return area
-}
+    let area = 3.14 * radius * radius;
+    return area;
+};
 
 console.log("Area of circle with radius 5 cm is :", areaOfCircle(5),"cm^2");
