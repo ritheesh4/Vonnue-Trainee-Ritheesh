d599ec2169d398e8811ad5c364d24e3a6e291527
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 15:45:06 +0530

    Removed unwanted variable declaration and added semicolon

--- 00037_leap_year_testing.js
+++ 00037_leap_year_testing.js
@@ -1,14 +1,12 @@
 // 37. Leap year or not
 
-let inputYear;
-
 const leapYearTest = (inputYear) => {
     if ((inputYear % 4 === 0) && (inputYear % 100 !== 0)) {
-        return "Leap Year"
+        return "Leap Year";
     } else {
-        return "Not a leap Year"
+        return "Not a leap Year";
     }
-}
+};
 
 console.log("The input year 2024 is : ", leapYearTest(2024));
 console.log("The input year 1900 is : ", leapYearTest(1900));
