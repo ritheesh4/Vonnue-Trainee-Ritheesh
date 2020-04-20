3cf5208d5d0327c7980114baaaf779d5fd2d72a8
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 12:50:32 +0530

    Removed unwanted function call

--- 0041_power.js
+++ 0041_power.js
@@ -2,14 +2,13 @@
 
 let inputNumber;
 let powerValue;
-let result =1;
+let result = 1;
 
 const powerOfNumber = (inputNumber, powerValue) => {
-    for (let i =0;i<powerValue;i++){
-        result = result* inputNumber
+    for (let i = 0; i < powerValue; i++) {
+        result = result * inputNumber
     }
     return result
 }
 
-powerOfNumber(inputNumber, powerValue);
-console.log("The value of 4 power 5: ", powerOfNumber(4,5))
+console.log("The value of 4 power 5: ", powerOfNumber(4, 5))
