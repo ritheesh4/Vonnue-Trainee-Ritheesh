59379ddf05c234d75a077d0b431ef295cdc58830
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:14:04 +0530

    removed unwanted variable declaration

--- 00019_vowel_consonant.js
+++ 00019_vowel_consonant.js
@@ -1,15 +1,14 @@
 // 19. Write a program to check if a character is a vowel or consonant
 
 const vowels = 'aeioAEIOU';
-let inputCharacter = "";
 
 const vowelOrConsonant = (inputCharacter) => {
     if (vowels.search(inputCharacter) >= 0) {
-        return "Vowel"
+        return "Vowel";
     } else {
-        return "Consonant"
+        return "Consonant";
     }
-}
+};
 
 console.log("The input character a is: ", vowelOrConsonant("a"));
 console.log("The input character b is : ", vowelOrConsonant("b"));
