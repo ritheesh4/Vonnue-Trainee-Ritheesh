e146f9a52c9c7d8338d7c42b56a2c1d800eb434b
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:29:33 +0530

    Removed unwanted declarations. Added semicolons. edited console message

--- 00010_sum_multiplication_n_choice.js
+++ 00010_sum_multiplication_n_choice.js
@@ -1,7 +1,5 @@
-// 10. Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.
-
-let n;
-let choiceOfUser; /*1.Muliplication or 2.Sum. Any other input is invalid.*/
+// 10. Write a program that asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n.
+/*choiceOfUser:1.Muliplication or 2.Sum. Any other input is invalid.*/
 
 const choiseOfUserInput = (choiceOfUser, n) => {
     if (choiceOfUser === 1) {
@@ -9,31 +7,31 @@ const choiseOfUserInput = (choiceOfUser, n) => {
     } else if (choiceOfUser === 2) {
         return sum(n)
     } else {
-        return invalidInput()
+        return invalidInput();
     }
-}
+};
 
 const sum = (n) => {
     let sumOfLimit = 0;
     for (let i = 1; i < n + 1; i++) {
         sumOfLimit = sumOfLimit + i;
     }
-    return sumOfLimit
-}
+    return sumOfLimit;
+};
 
 const multiplicaition = (n) => {
     let multiplicationOfLimit = 1;
     for (let i = 1; i < n + 1; i++) {
         multiplicationOfLimit = multiplicationOfLimit * i;
     }
-    return multiplicationOfLimit
-}
+    return multiplicationOfLimit;
+};
 
 const invalidInput = () => {
-    return "Invalid input"
-}
+    return "Invalid choice";
+};
 
-console.log("Sum of 10 numbers", choiseOfUserInput(2, 10));
-console.log("Multiplication of 10 numbers", choiseOfUserInput(1, 10));
-console.log("Multiplication of 10 numbers", choiseOfUserInput(3, 10));
+console.log("Sum of 10 numbers with choice 2:", choiseOfUserInput(2, 10));
+console.log("Multiplication of 10 numbers with choice 1:", choiseOfUserInput(1, 10));
+console.log("Multiplication of 10 numbers with choice 3:", choiseOfUserInput(3, 10));
 
