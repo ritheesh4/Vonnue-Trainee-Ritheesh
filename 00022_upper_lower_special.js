646c03335ce2fa60cebe7c89fef013d36e489829
Ritheesh <ritheesh.4@gmail.com>
2020-04-20 16:09:16 +0530

    edited console message

--- 00022_upper_lower_special.js
+++ 00022_upper_lower_special.js
@@ -1,19 +1,18 @@
 // 22. Write a program to check Uppercase, Lowercase or special character
 
-let inputCharacter = '';
 let asciiValueOfInput;
 
 const characterType = (inputCharacter) => {
     asciiValueOfInput = inputCharacter.charCodeAt();
     if (asciiValueOfInput >= 65 && asciiValueOfInput <= 90) {
-        return "Uppercase"
+        return "Uppercase";
     } else if (asciiValueOfInput >= 97 && asciiValueOfInput <= 122) {
-        return "Lowercase"
+        return "Lowercase";
     } else {
-        return "Special Character"
+        return "Special Character";
     }
-}
+};
 
-console.log("Input character a is: ", characterType('a'));
-console.log("Input $ is: ", characterType('$'));
-console.log("Input character A is: ", characterType('A'));
+console.log("Input character 'a' is: ", characterType('a'));
+console.log("Input '$' is: ", characterType('$'));
+console.log("Input character 'A' is: ", characterType('A'));
