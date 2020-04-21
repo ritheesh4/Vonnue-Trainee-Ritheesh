aa16c89a0faacd256a52c88cde639112c4e8b3a1
Ritheesh <ritheesh.4@gmail.com>
2020-04-21 16:03:31 +0530

    Changed function

---
+++ 00045_armstrong_interval.js
@@ -0,0 +1,22 @@
+// 45. Armstrong numbers between two intervals
+
+const armstrongNumbers = (inputNumber) => {
+    let addition = 0;
+    let remainder = 0;
+    let duplicate = inputNumber;
+
+    while (inputNumber > 0) {
+        remainder = inputNumber % 10;
+        addition = addition + remainder * remainder * remainder;
+        inputNumber = parseInt(inputNumber / 10);
+    }
+
+    if (addition === duplicate) {
+        return "Armstrong number";
+    } else {
+        return "Not armstrong number";
+    }
+};
+
+console.log("Armstrong number between the interval :", armstrongNumbers(153));
+console.log("Armstrong number between the interval :", armstrongNumbers(13));
