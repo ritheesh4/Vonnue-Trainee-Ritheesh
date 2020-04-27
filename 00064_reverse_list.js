92fb8641b810d60fa23056478b013fa986a48e4b
Ritheesh <ritheesh.4@gmail.com>
2020-04-27 15:28:20 +0530

    Initial commit

---
+++ 00064_reverse_list.js
@@ -0,0 +1,8 @@
+// 64. Write function that reverses a list, preferably in place.
+
+const reverseList = (input) => {
+    let reverse = input.reverse();
+    return reverse;
+};
+
+console.log("List = [2,7,4,3,5,6], reverse list = ",reverseList([2,7,4,3,5,6]));
