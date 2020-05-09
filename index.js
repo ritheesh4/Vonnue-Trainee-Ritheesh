0348ccf19ac2e82252ca7a843857160887792b6a
Ritheesh <ritheesh.4@gmail.com>
2020-05-09 09:51:35 +0530

    console added

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const writeIntoFile = () => {
     fs.writeFile("studentsList.json", JSON.stringify(studentsList), err => {
         if (err) throw err;
     });
-
 }
+
 // 1. Add student//     1. It should accept name, id
 const addStudent = (name, id) => {
     let studentsDetails = {
@@ -71,7 +71,7 @@ const changeClassTeacher = (className, newTeacherName) => {
         writeIntoFile();
         return "Done"
     }
-    
+
 };
 
 // 6. Remove a student from a class
@@ -82,8 +82,9 @@ const removeStudent = (className, id, studentName) => {
             writeIntoFile();
             return "Removed"
         }
-        return "Invalid input"
+
     }
+    return "Invalid input"
 };
 
 // 7. Delete a subject entry from every students
@@ -97,10 +98,8 @@ const deleteSubEntry = (subject) => {
         }
     }
 
-    fs.writeFile("studentsList.json", JSON.stringify(studentsList), err => {
-        if (err) throw err;
-        console.log("Done writing");
-    });
+   writeIntoFile();
+   return "Done"
 };
 
 // 8. Find the topper of a class given a subject
@@ -162,27 +161,16 @@ const studentTotalMark = (student) => {
     for (let i = 0; i < studentsList.students.length; i++) {
         if (studentsList.students[i].name === student) {
             studentId = i;
+           
             for (let j = 0; j < studentsList.students[i].marks.length; j++) {
                 totalMarks = totalMarks + studentsList.students[i].marks[j].mark;
             }
+            studentsList.students[i]['totalMarks'] = totalMarks
+            writeIntoFile();
+            break
         }
-        break
+       
     }
-
-    let userObject = {
-        name: student,
-        totalMarks: totalMarks,
-        id: studentId
-    }
-
-    fs.readFile('user.json', function (err, data) {
-        let json = JSON.parse(data);
-        json.push(userObject);
-        fs.writeFile("user.json", JSON.stringify(json), function (err) {
-            if (err) throw err;
-            console.log('Done');
-        });
-    })
     return totalMarks
 }
 
@@ -206,7 +194,14 @@ const addDivision = (name, teacherName, students) => {
 // console.log("Enter mark for a subject for multiple students by the given input subject name, and array of id and marks pairs:",subjectForMultipleStudents("Biology", [[56, 90], [57, 80],[58,100]]));
 // console.log("Edit mark for a particular subject of a student. Input id, Subject and marks:", editMarks(55, "Biology", 80));
 // console.log("Change class teacher name. Input class and new teacher name:", changeClassTeacher("class A", "Anoop"))
-console.log("Remove a student from a class:\n",removeStudent("class A", 56, "renjisha"))
+// console.log("Remove a student from a class:\n",removeStudent("class A", 57, "renjith"))
+// console.log("Delete a subject entry from every students. Input subject:\n", deleteSubEntry("malayalam"));
+// console.log(" Find the topper of a class given a subject. Input subject:\n",topperOfClass("Biology"));
+// console.log("Find the average mark for a given subject. Input the subject:\n",avgOfSubject("malayalam"));
+// console.log(" Sort and display the list of students in any order",sortStudents());
+console.log("Find the total mark for a student and save it in the user object",studentTotalMark("renjisha"))
+
+
 // console.log(studentTotalMark('renjisha'))
 // console.log(sortStudents())
 
